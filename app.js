955df540c19b73e1830accf8e214650e272404a6
kazi bablu <bablu@shuttlebd.com>
2022-04-16 02:19:42 +0600

    computed properties

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
           showBooks:true,
           url:'www.fightfor.com',
           books:[
-              {title: 'name of the wind',author:'nazrul',img:'asset/1.jpg'},
-              {title: 'name of the snow',author:'tiger',img:'asset/1.jpg'},
-              {title: 'name of the white',author:'mr Fox',img:'asset/1.jpg'}
+              {title: 'name of the wind',author:'nazrul',img:'asset/1.jpg',isFav:true},
+              {title: 'name of the snow',author:'tiger',img:'asset/1.jpg',isFav:false},
+              {title: 'name of the white',author:'mr Fox',img:'asset/1.jpg',isFav:true}
 
           ]
       }
@@ -16,7 +16,15 @@ const app = Vue.createApp({
       {
           this.showBooks = !this.showBooks
       },
+      toggleFav(book){
+        book.isFav =! book.isFav
+      }
     
+  },
+  computed:{
+      filteredBooks(){
+          return this.books.filter((book )=> book.isFav)
+      }
   }
 })
 
